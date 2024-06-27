33f3036c249961a817ea739b4b568efc4227ecf8
Yes2eul <yeseul5611@gmail.com>
2024-06-27 14:59:53 +0900

    feat: 소개 추가

--- components/madeBy.js
+++ components/madeBy.js
@@ -14,7 +14,14 @@ const MadeBy = () => {
             height={400}
             className="rounded-xl"
           />
-          <span className="text-white block mt-4">신예슬</span>
+          <span className="text-white block my-4">신예슬</span>
+          <span className="text-textActive text-xs">
+            수고한 그대들에게 박수를 짝짝짝 <br />
+            끝난 줄 알았죠? 이제 시작이에요~! <br />
+            <span className="text-textInactive block italic text-xs mt-4">
+              #ISTJ #멱살잡고 #끌고가는 #리더
+            </span>
+          </span>
         </li>
         <li className="text-center">
           <Image
@@ -24,7 +31,14 @@ const MadeBy = () => {
             height={400}
             className="rounded-xl"
           />
-          <span className="text-white block mt-4">최성원</span>
+          <span className="text-white block my-4">최성원</span>
+          <span className="text-textActive text-xs">
+            벌려놓은 일들에 끌려다니는 <br />
+            똑부러지고 싶은 갓생 호소인 <br />
+            <span className="text-textInactive block italic text-xs mt-4">
+              #eNtP #이러다내가 #똑 #부러지겠어
+            </span>
+          </span>
         </li>
         <li className="text-center">
           <Image
@@ -34,7 +48,14 @@ const MadeBy = () => {
             height={400}
             className="rounded-xl"
           />
-          <span className="text-white block mt-4">신민규</span>
+          <span className="text-white block my-4">신민규</span>
+          <span className="text-textActive text-xs">
+            GPT에 점령 당한 사람 <br />
+            <br />
+            <span className="text-textInactive block italic text-xs mt-4">
+              #iNtP #GPT없인 #못살게돼버린 #몸
+            </span>
+          </span>
         </li>
         <li className="text-center">
           <Image
@@ -44,7 +65,14 @@ const MadeBy = () => {
             height={400}
             className="rounded-xl"
           />
-          <span className="text-white block mt-4">김채린</span>
+          <span className="text-white block my-4">김채린</span>
+          <span className="text-textActive text-xs">
+            GPT야 그동안 멍청하다고 놀려서 미안해 <br />
+            개발할 때 모르는 거 알려줘서 고마워 . . <br />
+          </span>
+          <span className="text-textInactive block italic text-xs mt-4">
+            #eNTP #에러뜨면 #눈물나는 #막내
+          </span>
         </li>
       </ul>
     </div>
--- components/shared/footer.js
+++ components/shared/footer.js
@@ -5,7 +5,14 @@ const Footer = () => {
     <footer>
       <div className="flex justify-center items-center px-auto py-4 mt-8">
         <span className="text-textInactive text-xs">
-          &copy; 2024 ESG SW 엔지니어 트렉 1조. All rights reserved.
+          <a
+            href="https://west-orbit-b88.notion.site/Moving-Movie-c13f63ffd4c9412780175efa94e73b9b"
+            target="_blank"
+            className="hover:cursor-pointer hover:text-textActive transition duration-300 ease-in-out"
+          >
+            &copy; 2024 ESG SW 엔지니어 트렉 1조.
+          </a>{" "}
+          All rights reserved.
         </span>
       </div>
     </footer>
