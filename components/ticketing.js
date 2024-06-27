03156000db6f650a564a5949c61dea4529e36fc4
Yes2eul <yeseul5611@gmail.com>
2024-06-27 15:16:06 +0900

    fix: 글씨 깨짐 수정

--- components/ticketing.js
+++ components/ticketing.js
@@ -10,7 +10,7 @@ const Ticketing = () => {
         rel="noopener noreferrer"
         className=" py-2 px-4 rounded mr-4 p-2"
       >
-        <img src="/cgv.png" height={50} width={50} />
+        <img src="/cgv.png" width={50} />
       </a>
       <a
         href="https://www.megabox.co.kr"
@@ -18,7 +18,7 @@ const Ticketing = () => {
         rel="noopener noreferrer"
         className=" py-2 px-4 rounded mr-4"
       >
-        <img src="/megabox.png" height={100} width={100} />
+        <img src="/megabox.png" width={100} />
       </a>
       <a
         href="https://www.lottecinema.co.kr"
@@ -26,7 +26,7 @@ const Ticketing = () => {
         rel="noopener noreferrer"
         className=" py-2 px-4 rounded "
       >
-        <img src="/lottecinema.png" height={150} width={150} />
+        <img src="/lottecinema.png" width={140} />
       </a>
     </div>
   );
